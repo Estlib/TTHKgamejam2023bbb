35ae1ed84a9a22463a3227ef1052c45b78432ab5
Estlib <75094683+Estlib@users.noreply.github.com>
2024-10-23 14:59:00 +0300

    Initial commit

---
+++ TTHKgamejam2023/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TTHKgamejam2023.Models;
+
+namespace TTHKgamejam2023.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+        public IActionResult GameJam2023()
+        {
+            return View();
+        }
+        public IActionResult ItchIo()
+        {
+            return View();
+        }
+        public IActionResult Voting()
+        {
+            return View();
+        }
+        public IActionResult Results()
+        {
+            return View();
+        }
+        public IActionResult SecretPage()
+        {
+            return View();
+        }
+        public IActionResult Projects2023()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TTHKgamejam2023/Controllers/ShowcaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TTHKgamejam2023.Controllers
+{
+    public class ShowcaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
